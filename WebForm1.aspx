6598b9d5ef587698dca327cf13a6655ddfa30d74
Karnika Shukla <50082509+karnikashukla@users.noreply.github.com>
2020-02-05 10:29:40 +0530

    Add files via upload

---
+++ WebForm1.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="true" Height="147px" Width="273px">
+            </asp:GridView>
+        </div>
+        <asp:DataList ID="DataList1" runat="server">
+            <ItemTemplate>  
+                        <div >  
+                            <h4><%#Eval("id")%></h4>  
+                            <h6><%#Eval("name")%></h6>  
+                            <h6><%#Eval("mobile_number")%></h6> 
+                        </div>  
+                     
+                </ItemTemplate> 
+        </asp:DataList>
+        
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+            SqlConnection connection = new SqlConnection(@"Data Source=(localdb)\ProjectsV13;Initial Catalog=New_Database;Integrated Security=True");
+            DataSet ds = new DataSet();
+            SqlDataAdapter dataAdapter = new SqlDataAdapter("select * from mytable",connection);
+            
+            dataAdapter.Fill(ds);
+           
+            GridView1.DataSource = ds;
+            GridView1.DataBind();
+
+            SqlCommand cmd = new SqlCommand("mytable", connection);
+            cmd.CommandType = CommandType.StoredProcedure;
+            connection.Open();
+            DataList1.DataSource = ds;
+            DataList1.DataBind();
+        }
+    }
+}
